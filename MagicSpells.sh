e124a874c957dd379a66c980236b76304b1a497b
EUye9IM <root@rnvps-51>
2023-04-04 23:08:24 +0800

    test mahoo

--- MagicSpells.sh
+++ MagicSpells.sh
@@ -7,5 +7,5 @@ git remote set-url other git@github.com:EUye9IM/${1}.git && \
 	mv $f ${1} && git add . && \
 	git commit -m "[REF] move" && \
 	git checkout main && \
-	git merge --allow-unrelated-histories other -m "merge" && \
-	git push
+	git merge --squash --allow-unrelated-histories other -m "merge" 
+#	git push
